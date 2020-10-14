1b1dd153fdd3c6776fd1c44261366afd25c10a38
Caryn Keffer <carynkeffer@gmail.com>
2020-10-13 20:58:39 -0400

    Add health attribute

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -13,6 +13,6 @@ class ShipTest < Minitest::Test
    assert_instance_of Ship, @cruiser
    assert_equal "Cruiser", @cruiser.name
    assert_equal 3, @cruiser.length
-   # assert_equal 3, @cruiser.health
+   assert_equal 3, @cruiser.health
   end
 end
