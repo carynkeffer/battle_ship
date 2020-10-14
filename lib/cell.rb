4fea28785471a08f9eee641598d2463778ebe773
Caryn Keffer <carynkeffer@gmail.com>
2020-10-14 19:56:26 -0400

    Stil need to fix render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -23,6 +23,61 @@ class Cell
   def fire_upon
     if @ship.hit
      @fired_upon = true
-   end
+   else
+     @fired_upon = false
+    end
   end
+
+  def render
+    if @fired_upon == false
+      "."
+    elsif @fire_upon == false
+      "M"
+    end
+  end
+
 end
+
+# pry(main)> cell_1 = Cell.new("B4")
+# # => #<Cell:0x00007f84f11df920...>
+#
+# pry(main)> cell_1.render
+# # => "."
+#
+# pry(main)> cell_1.fire_upon
+#
+# pry(main)> cell_1.render
+# # => "M"
+#
+# pry(main)> cell_2 = Cell.new("C3")
+# # => #<Cell:0x00007f84f0b29d10...>
+#
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# # => #<Ship:0x00007f84f0ad4fb8...>
+#
+# pry(main)> cell_2.place_ship(cruiser)
+#
+# pry(main)> cell_2.render
+# # => "."
+#
+# # Indicate that we want to show a ship with the optional argument
+# pry(main)> cell_2.render(true)
+# # => "S"
+#
+# pry(main)> cell_2.fire_upon
+#
+# pry(main)> cell_2.render
+# # => "H"
+#
+# pry(main)> cruiser.sunk?
+# # => false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# # => true
+#
+# pry(main)> cell_2.render
+# # => "X"
