00d18b91b3a417300d347f6e30306408b5b64a26
Caryn Keffer <carynkeffer@gmail.com>
2020-10-14 18:22:39 -0400

    Add empty? and place_ship methods and tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,38 +6,13 @@ class Cell
     @coordinate = coordinate
     @ship = nil
   end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(cruiser)
+    @ship = cruiser
+  end
+
 end
-
-
-
-
-
-
-# pry(main)> require './lib/ship'
-# # => true
-#
-# pry(main)> require './lib/cell'
-# # => true
-#
-# pry(main)> cell = Cell.new("B4")
-# # => #<Cell:0x00007f84f0ad4720...>
-#
-# pry(main)> cell.coordinate
-# # => "B4"
-#
-# pry(main)> cell.ship
-# # => nil
-#
-# pry(main)> cell.empty?
-# # => true
-#
-# pry(main)> cruiser = Ship.new("Cruiser", 3)
-# # => #<Ship:0x00007f84f0891238...>
-#
-# pry(main)> cell.place_ship(cruiser)
-#
-# pry(main)> cell.ship
-# # => #<Ship:0x00007f84f0891238...>
-#
-# pry(main)> cell.empty?
-# # => false
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -14,6 +14,20 @@ class Test < Minitest::Test
 
     assert_instance_of Cell, @cell
     assert_equal "B4", @cell.coordinate
-    assert_equal nil, @cell.ship
+    # assert_equal nil, @cell.ship
+    assert_nil @cell.ship
+  end
+
+  def test_is_it_empty
+
+    assert_equal true, @cell.empty?
+  end
+
+  def test_place_ship
+
+    @cell.place_ship(@cruiser)
+
+    assert_equal @cruiser, @cell.ship
+    assert_equal false, @cell.empty?
   end
 end
