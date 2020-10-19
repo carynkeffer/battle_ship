cbb09818f58a3901a9fad60461939a821e5c312c
Joseph Budina <joebud3@gmail.com>
2020-10-18 19:12:28 -0600

    Add place method and passing test

--- test/board_test.rb
+++ test/board_test.rb
@@ -55,7 +55,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_ships_can_be_placed
-    # require "pry"; binding.pry
+    
     @board.place(@cruiser, ["A1", "A2", "A3"])
     cell_1 = @board.cells["A1"]
     cell_2 = @board.cells["A2"]
@@ -63,8 +63,7 @@ class BoardTest < Minitest::Test
     cell_1.ship
     cell_2.ship
     cell_3.ship
-require "pry"; binding.pry
+
     assert_equal true, cell_3.ship == cell_2.ship
   end
-
 end
